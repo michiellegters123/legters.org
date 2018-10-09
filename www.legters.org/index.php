fd77754ad9a0f2ea7bd019934aaf9c3297643375
nathan kolpa <nkolpa12@gmail.com>
2018-10-09 19:55:55 +0200

    added basic front page

--- www.legters.org/index.php
+++ www.legters.org/index.php
@@ -1,3 +1,25 @@
-<?php
+<html>
 
+    <head>
+        <link href="FrontPage.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
 
+        <div class="CenterDiv">
+            <div style="width: 100%">
+                <ul id="HostNames">
+                    <li>
+                        <div class="TextCenterDiv"><span>Michiel</span></div>
+                    </li>
+                    <li>
+                        <div class="TextCenterDiv"><span>Nathan</span></div>
+                    </li>
+                    <li>
+                        <div class="TextCenterDiv"><span>Nicc</span></div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    </body>
+</html>
